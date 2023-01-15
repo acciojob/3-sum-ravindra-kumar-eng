737e18ea2578c1c177747182978dd1fe4d7a9f7e
ravindra-kumar-eng <117750035+ravindra-kumar-eng@users.noreply.github.com>
2023-01-15 16:33:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function threeSum(arr, target) {
+function threeSum(arr, x) {
   //your code here
+	let closedtsum=Number.MAX_VALUE;
+	for(let i=0;i<arr.length;i++){
+		for(let j=i+1;j<arr.length;j++){
+			for(let k=j+1;k<arr.length;k++){
+				if(Math.abs(x-closedtsum)>Math.abs(x-(arr[i]+arr[j]+arr[k])))
+					closedtsum=(arr[i]+arr[j]+arr[k]);
+			}
+		}
+	}
+	return closedtsum
 }
 
 module.exports = threeSum;
